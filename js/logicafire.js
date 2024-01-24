643e4f8f02bbb0ae1fffa4f16b1f6395dcfd86ce
Arthur P. Nunes <arthurpessoanunes@gmail.com>
2024-01-24 08:27:32 -0300

    up

--- js/logicafire.js
+++ js/logicafire.js
@@ -1,44 +1,76 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const formulario1 = document.getElementById('formulario1');
-  const formulario2 = document.getElementById('formulario2');
-  const formulario3 = document.getElementById('formulario3');
 
-  if (formulario1) {
-    formulario1.addEventListener('submit', function (e) {
-      e.preventDefault();
-      console.log('Formulário 1 submetido');
-      const cadastrador = document.getElementById('cadastrador').value;
-      const n_cliente = document.getElementById('n_cliente').value;
-      const numero = document.getElementById('numero').value;
-      const cpf = document.getElementById('cpf').value;
-      const endereco = document.getElementById('endereco').value;
-      const data_nasc = document.getElementById('data_nasc').value; 
+import  firebase  from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
 
-      // Enviar dados para o Firebase
-      console.log('Enviando dados para Firebase...');
-      firebase.database().ref('/logicafire.js').push({ dados: 'exemplo' })
-  .then(() => console.log('Dados enviados com sucesso'))
-  .catch(error => console.error('Erro ao enviar dados:', error));
+console.log('Iniciando Firebase...');
+const firebaseConfig = {
+  apiKey: "AIzaSyADVbHLFCeBiisgQap6GE53Q_8LmKMq91c",
+  authDomain: "terra-crm93.firebaseapp.com",
+  databaseURL: "https://terra-crm93-default-rtdb.firebaseio.com",
+  projectId: "terra-crm93",
+  storageBucket: "terra-crm93.appspot.com",
+  messagingSenderId: "550321428380",
+  appId: "1:550321428380:web:12ed780ce3c3ca145c1a59"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.initializeApp(firebaseConfig);
+
+// Verificação do estado de autenticação
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    console.log('Usuário conectado:', user.uid);
+  } else {
+    console.log('Usuário desconectado.');
+  }
+});
+
+function enviarForm1(){
+  const cadastrador = document.getElementById('cadastrador')
+  const n_cliente = document.getElementById('n_cliente')
+  const numero = document.getElementById('numero')
+  const cpf = document.getElementById('cpf')
+  const endereco = document.getElementById('endereco')
+  const data_nasc = document.getElementById('data_nasc')
+  
+    console.log('Formulário 1 submetido');
+    if(cadastrador&&n_cliente&&numero&&cpf&&endereco&&data_nasc){
+      const dados = {
+        cadastrador: cadastrador.value,
+        n_cliente:n_cliente.value,
+        numero: numero.value,
+        cpf:cpf.value,
+        endereco:endereco.value,
+        data_nasc:data_nasc.value
+      }
+      db.collection('formulario1').add(dados).then((docRef)=>{
+        console.log('Doc add', docRef);
+        cadastrador.value='';
+        n_cliente.value='';
+        numero.value='';
+        cpf.value='';
+        endereco.value='';
+        data_nasc.value='';
+      }).catch((error)=>{
+        console.error('Erro add', error)
+      })}
+}
 
   if (formulario2) {
-    formulario2.addEventListener('submit', function (e) {
+    formulario2.addEventListener('onclick', function (e) {
       e.preventDefault();
+      console.log('Formulário 2 submetido');
       const cadastrador1 = document.getElementById('cadastrador1').value;
       const nome_cliente = document.getElementById('nome_cliente1').value;
       const descricao = document.getElementById('descricao').value;
       const interesse = document.getElementById('interesse').value;
       const negocio = document.getElementById('negocio').value;
       const contato = document.getElementById('contato').value;
-
-      // Enviar dados para o Firebase
-      firebase.database().ref('/logicafire.js').push({ dados: 'exemplo' })
-  .then(() => console.log('Dados enviados com sucesso'))
-  .catch(error => console.error('Erro ao enviar dados:', error));
-
-
+    })}
   if (formulario3) {
-    formulario3.addEventListener('submit', function (e) {
+    formulario3.addEventListener('onclick', function (e) {
       e.preventDefault();
+      console.log('Formulário 3 submetido');
       const cadastrador2 = document.getElementById('cadastrador2').value;
       const n_cliente2 = document.getElementById('n_cliente2').value;
       const tipo = document.getElementById('tipo').value;
@@ -48,11 +80,5 @@ document.addEventListener('DOMContentLoaded', function () {
       const cidade = document.getElementById('cidade').value;
       const endereco = document.getElementById('endereco').value;
       const data_inc = document.getElementById('data_inc').value;
+  })}
 
-      // Enviar dados para o Firebase
-      firebase.database().ref('/logicafire.js').push({ dados: 'exemplo' })
-  .then(() => console.log('Dados enviados com sucesso'))
-  .catch(error => console.error('Erro ao enviar dados:', error));
-
-  }
-})
